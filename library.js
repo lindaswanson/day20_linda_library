823b866530b8c9398a0ae8d168943577d4ae1541
Linda Swanson <linda.swanson@detroitlabs.com>
2015-03-20 09:34:29 -0400

    Initial comment describing requirements for the Library program.

---
+++ library.js
@@ -0,0 +1,21 @@
+/*
+Author: Linda Swanson
+Date: 20150320
+
+The Library
+
+You've been contracted to write a piece of software for the local library.
+The software needs to meet the following requirements:
+
+- The software should track the library's inventory of books
+
+- Each book listing should include the title, author, genre (Fiction, Non-Fiction, etc.), length,
+and checked-in/checked-out status
+
+- Librarians should be able to:
+a.) add books to the inventory
+b.) remove books from the inventory
+c.) view a list of all the books in the library
+d.) view a list of all the books in a given genre
+e.) search for a book by title or author
+*/
